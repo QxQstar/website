774fe49ba66881ea3496a331e2a2d92ca7fb1330
QxQstar <1395294694@qq.com>
2016-09-09 17:57:00 +0800

    安装gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/9/9.
+ */
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+gulp.task('default',function(){
+});
